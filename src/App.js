01aa41bd704ec23473a307e8c1bee0ed6a86676e
emo_new_bug <emotupev11@gmail.com>
2023-02-12 15:09:40 +0200

    fix: css selectors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import "./App.css";
 
+const styles = {
+  App: {
+    maxWidth: '60rem',
+    margin: '2rem auto',
+    padding: '1rem',
+    boxShadow: '0 2px 8px rgba(0, 0, 0, 0.25)',
+    borderRadius: '14px',
+    backgroundColor: 'white'
+  },
+  div: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '1rem',
+    paddingBottom: '1rem',
+    borderBottom: '1px solid #ccc',
+    h3: {
+      margin: '0 0 0.25rem 0'
+    },
+    'nth-of-type:(1)': {
+      fontStyle: "italic"
+    },
+    'nth-of-type:(2)': {
+      marginTop: '0.25rem',
+      fontWeight: 'bold',
+      color: '#ad5502',
+      fontSize: '1.25rem',
+    }
+  },
+
+
+}
+
 function App() {
   return (
-    <div style={{
-      maxWidth: '60rem',
-  margin: '2rem auto',
-  padding: '1rem',
-  boxShadow: '0 2px 8px rgba(0, 0, 0, 0.25)',
-  borderRadius: '14px',
-  backgroundColor: 'white'
-    }}>
-      <div style={{
-         display: 'flex',
-         justifyContent: 'space-between',
-         margin: '1rem',
-         paddingBottom: '1rem',
-         borderBottom: '1px solid #ccc',
-      }}>
-        <h3 style={{ margin: '0 0 0.25rem 0'}}>Sushi</h3>
-        <div style={{fontStyle: "italic"}}>I prefer steak than sushi.</div>
-        <div style={{
-           marginTop: '0.25rem',
-           fontWeight: 'bold',
-           color: '#ad5502',
-           fontSize: '1.25rem',
-        }}>$9.99</div>
+    <div style={styles.App}>
+      <div style={styles.div}>
+        <h3 style={styles.div.h3}>Sushi</h3>
+        <div style={styles.div["nth-of-type(1)"]}>I prefer steak than sushi.</div>
+        <div style={styles.div["nth-of-type:(2)"]}>$9.99</div>
       </div>
     </div>
   );
